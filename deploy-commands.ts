6f10783fe751848de9966988382a65f8556e772c
aqua <norbertzieba06@gmail.com>
2023-10-23 12:09:38 -0400

    we're back

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -8,7 +8,61 @@ const commands = [
     name: 'ping',
     description: 'Replies with Pong!',
   },
+  {
+    name: 'server',
+    description: "Replies with server info."
+  },
+  {
+    name: "beep",
+    description: "Replies with Boop!"
+  },
+  {
+    name: "user-info",
+    description: "Replies with user info."
+  },
+  {
+    name: "remindme",
+    description: "Reminds user in X minutes.",
+    options: [
+      {
+        name: "minutes",
+        description: "Number of minutes",
+        type: 4, // 4 corresponds to integer type
+        required: true
+      }
+    ]
+  },
+  {
+    name: "tasks",
+    description: "Shows user tasks."
+  },
+  {
+    name: "add-task",
+    description: "Add task to user list.",
+    options: [
+      {
+        name: "name",
+        description: "Name of the task",
+        type: 3, // 3 corresponds to string type
+        required: true
+      }
+    ]
+  },
+  {
+    name: "remove-task",
+    description: "Removes task from user list.",
+    options: [
+      {
+        name: "id",
+        description: "Id of the task",
+        type: 4, // 4 corresponds to integer type
+        required: true
+      }
+    ]
+  }
 ];
+
+
 
 const rest = new REST({ version: '10' }).setToken(env.DISCORD_TOKEN);
 
--- index.ts
+++ index.ts
@@ -1,29 +1,119 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { env } from "./utils/config";
+import { Client, Events, GatewayIntentBits, userMention } from 'discord.js';
+import { env } from './utils/config';
+import prisma from './utils/prisma';
+import command from './commands/ping';
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		interaction.reply('Pong.');
-	} else if (commandName === 'beep') {
-		interaction.reply('Boop.');
-	} else if (commandName === 'server') {
-		interaction.reply(`Guild name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`);
-	} else if (commandName === 'user-info') {
-		interaction.reply('Your username: ' + interaction.user.username + '\nYour ID: ' + interaction.user.id);
-	}
+  if (commandName === 'ping') {
+    interaction.reply('Pong.');
+  } else if (commandName === 'beep') {
+    interaction.reply('Boop.');
+  } else if (commandName === 'server') {
+    interaction.reply(
+      `Guild name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`
+    );
+  } else if (commandName === 'user-info') {
+    interaction.reply(
+      'Your username: ' +
+      interaction.user.username +
+      '\nYour ID: ' +
+      interaction.user.id
+    );
+  } else if (commandName === 'remindme') {
+    const time = interaction.options.getInteger('minutes');
+
+    interaction.reply(
+      `Reminder for ${userMention(interaction.user.id)} created, ${time} minutes.`
+    );
+
+    setTimeout(() => {
+      interaction.followUp(
+        `Come back to work ${userMention(interaction.user.id)}.`
+      );
+    }, time! * 60 * 1000);
+  } else if (commandName === 'tasks') {
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId: interaction.user.id
+      }
+    });
+
+    let data = `### Your tasks ${userMention(interaction.user.id)}\n`;
+
+    // data += '----------------------------------------------------\n';
+
+    tasks.forEach((item, idx) => {
+      data += ` ${idx + 1}. ${item.content}` + "\n";
+    })
+
+    interaction.reply(data);
+
+  }
+
+  else if (commandName === 'add-task') {
+    const taskName = interaction.options.getString('name');
+
+
+    if (!taskName) {
+      interaction.reply(`Task "${taskName}" has been not created, add task name.`);
+      return;
+    }
+
+    await prisma.task.create({
+      data: {
+        userId: interaction.user.id,
+        content: taskName
+      }
+    });
+
+    interaction.reply(`Task "${taskName}" has been created.`);
+  }
+
+  else if (commandName === 'remove-task') {
+    const taskId = interaction.options.getInteger('id');
+
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId: interaction.user.id
+      }
+    });
+
+
+    if (!taskId) {
+      interaction.reply(`Task "${taskId}" has been not deleted, add task od.`);
+      return;
+    }
+
+    try {
+      await prisma.task.delete({
+        where: {
+          id: tasks[taskId - 1].id
+        }
+      })
+    } catch (error) {
+      console.log(error);
+      interaction.reply("Something went wrong");
+      return;
+    }
+
+
+
+    interaction.reply(`Task "${taskId}" has been deleted.`);
+  }
+
 });
 
 // Log in to Discord with your client's token
